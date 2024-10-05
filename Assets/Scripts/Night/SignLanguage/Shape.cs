4a045f22d760379b9173a4b584b3502ff5302eff
김영찬 <small1258@naver.com>
2024-10-05 20:43:50 +0900

    10.05Work
    밤 시스템 컷씬을 위한 대략적인 스크립트 구조 제작.

---
+++ Assets/Scripts/Night/SignLanguage/Shape.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HandByHand.NightSystem.SignLanguageSystem
+{
+    public class Shape : MonoBehaviour
+    {
+
+    }
+}
