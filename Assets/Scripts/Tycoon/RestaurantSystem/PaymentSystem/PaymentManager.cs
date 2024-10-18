d38bb4ad7c531f80732a7d7d02110de169504a90
es-e2b <ensu1997@naver.com>
2024-10-19 00:57:51 +0900

    Update PaymentManager.cs

--- Assets/Scripts/Tycoon/RestaurantSystem/PaymentSystem/PaymentManager.cs
+++ Assets/Scripts/Tycoon/RestaurantSystem/PaymentSystem/PaymentManager.cs
@@ -1,13 +1,41 @@
 namespace Assets.Scripts.Tycoon.RestaurantSystem.PaymentSystem
 {
+    using System.Collections;
     using System.Collections.Generic;
+    using System.Linq;
     using UnityEngine;
+    using UnityEngine.Events;
 
     public class PaymentManager : MonoBehaviour
     {
         public static PaymentManager Instance { get; private set; }
         private Queue<Customer> WaitingCustomer;
-        private Customer paymentCustomer;
+        private int _totalPrice;
+        private int TotalPrice
+        {
+            get => _totalPrice;
+            set
+            {
+                _totalPrice = value;
+                OnChangedTotalPrice.Invoke(value);
+            }
+        }
+        private Customer _payingCustomer;
+        public Customer PayingCustomer
+        {
+            get => _payingCustomer;
+            set
+            {
+                _payingCustomer=value;
+                OnChangedCustomer.Invoke(value);
+                if(value==null)
+                {
+                    StartCoroutine(WaitPayingCustomer());
+                    return;
+                }
+                TotalPrice=value.OrderMenus.Sum(menu => menu.Price);
+            }
+        }
         private void Awake()
         {
             if (Instance != null && Instance != this)
@@ -17,15 +45,29 @@ namespace Assets.Scripts.Tycoon.RestaurantSystem.PaymentSystem
             }
             Instance = this;
             WaitingCustomer=new();
+            OnChangedCustomer=new();
+            OnChangedTotalPrice=new();
         }
         public void AddWaitingCustomer(Customer customer)
         {
-            if(paymentCustomer==null)
+            if(PayingCustomer==null)
             {
-                paymentCustomer=customer;
+                PayingCustomer=customer;
                 return;
             }
             WaitingCustomer.Enqueue(customer);
         }
+        private IEnumerator WaitPayingCustomer()
+        {
+            yield return new WaitUntil(() => WaitingCustomer.Count>0);
+            PayingCustomer=WaitingCustomer.Dequeue();
+        }
+        private void ChangeToNextCustomer()
+        {
+            if(WaitingCustomer.Count>0) PayingCustomer=WaitingCustomer.Dequeue();
+            else PayingCustomer=null;
+        }
+        public UnityEvent<Customer> OnChangedCustomer { get; private set; }
+        public UnityEvent<int> OnChangedTotalPrice { get; private set; }
     }
 }
