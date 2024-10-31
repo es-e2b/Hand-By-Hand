3f94c88d0e2a6a63f5337f025d79e7fd4302eca2
es-e2b <ensu1997@naver.com>
2024-11-01 00:45:35 +0900

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,11 +2,32 @@ namespace Assets.Scripts
 {
     using UnityEngine;
     using CharacterData;
+    using UnityEngine.Events;
 
     public class GameManager : MonoBehaviour
     {
         public static GameManager Instance { get; private set; }
         public Character[] CharacterPool;
+        private int _dayCount;
+        public int DayCount
+        {
+            get => _dayCount;
+            set
+            {
+                _dayCount = value;
+                OnChangedDayCount.Invoke(value);
+            }
+        }
+        private int _dailySales;
+        public int DailySales
+        {
+            get => _dailySales;
+            set
+            {
+                _dailySales = value;
+                OnChangedDailySales.Invoke(value);
+            }
+        }
         private void Awake()
         {
             if (Instance != null && Instance != this)
@@ -17,5 +38,13 @@ namespace Assets.Scripts
             Instance = this;
             DontDestroyOnLoad(this);
         }
+        private void Start()
+        {
+            DayCount=1;
+        }
+        [field:SerializeField]
+        public UnityEvent<int> OnChangedDayCount { get; private set; }
+        [field:SerializeField]
+        public UnityEvent<int> OnChangedDailySales { get; private set; }
     }
 }
