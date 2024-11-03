a45b80751324295563ac7bea2a7bd9526a47c430
es-e2b <ensu1997@naver.com>
2024-11-03 16:56:02 +0900

    Update OrderingCustomerController.cs

--- Assets/Scripts/Tycoon/RestaurantSystem/OrderSystem/OrderingCustomerController.cs
+++ Assets/Scripts/Tycoon/RestaurantSystem/OrderSystem/OrderingCustomerController.cs
@@ -59,7 +59,16 @@ namespace Assets.Scripts.Tycoon.RestaurantSystem.OrderSystem
             yield return new WaitUntil(()=>OrderManager.Instance!=null);
             customerUI=gameObject;
             OrderManager.Instance.OnChangedCustomer.AddListener((customer)=>Customer=customer);
-            OrderManager.Instance.OnCorrectAnswer.AddListener((index)=>orderMenuButtons[index].GetComponent<OrderMenuButton>().RemoveQuestionMark());
+            OrderManager.Instance.OnCorrectAnswer.AddListener((index)=>
+            {
+                orderMenuButtons[index].GetComponent<OrderMenuButton>().RemoveQuestionMark();
+                if(++index>=Customer.OrderMenus.Length)
+                {
+                    OrderManager.Instance.OnAllCrrectAnswer();
+                    return;
+                }
+                orderMenuButtons[index].GetComponent<OrderMenuButton>().CheckOrderMenuAction.Invoke(Customer.OrderMenus[index], index);
+            });
             foreach(GameObject orderMenuButton in orderMenuButtons)
             {
                 orderMenuButton.GetComponent<OrderMenuButton>().CheckOrderMenuAction=CheckOrderMenu;
