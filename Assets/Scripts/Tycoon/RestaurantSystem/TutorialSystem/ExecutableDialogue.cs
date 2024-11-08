a4e635576767d32754a44c1829d4f04ad9909e2e
es-e2b <ensu1997@naver.com>
2024-11-08 11:46:32 +0900

    fix: 태그에 따른 글자색 적용시 느리게 출력되던 버그 수정

--- Assets/Scripts/Tycoon/RestaurantSystem/TutorialSystem/ExecutableDialogue.cs
+++ Assets/Scripts/Tycoon/RestaurantSystem/TutorialSystem/ExecutableDialogue.cs
@@ -71,20 +71,25 @@ namespace Assets.Scripts.Tycoon.RestaurantSystem.TutorialSystem
                 yield return null;
                 elapseTime+=Time.deltaTime;
                 int nextIndex=(int)(elapseTime*_charactersPerSecond);
+                bool onTag=false;
+                int tagNumber=0;
                 for(;currentIndex<=nextIndex&&currentIndex<_currentString.Length;currentIndex++)
                 {
                     stringBuilder.Append(_currentString[currentIndex]);
                     if(_currentString[currentIndex]=='<')
                     {
-                        while (true)
-                        {
-                            currentIndex++;
-                            stringBuilder.Append(_currentString[currentIndex]);
-                            if(_currentString[currentIndex]=='>')
-                            {
-                                break;
-                            }   
-                        }
+                        onTag=true;
+                    }
+                    else if(_currentString[currentIndex]=='>')
+                    {
+                        onTag=false;
+                        elapseTime+=(float)tagNumber/_charactersPerSecond;
+                        tagNumber=0;
+                    }
+                    if(onTag)
+                    {
+                        nextIndex++;
+                        tagNumber++;
                     }
                 }
                 _text.text=stringBuilder.ToString();
