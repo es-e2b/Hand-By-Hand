79e4fbe128c7dae4816d5678b6e41cb7c05f5ce6
es-e2b <ensu1997@naver.com>
2024-11-09 06:04:10 +0900

    fix: ExecutableElement 기본 동작 추가(스킵, 선택에 따라 클릭 할 때까지 기다리기)

--- Assets/Scripts/Tycoon/RestaurantSystem/TutorialSystem/ExecutableElement.cs
+++ Assets/Scripts/Tycoon/RestaurantSystem/TutorialSystem/ExecutableElement.cs
@@ -3,46 +3,85 @@ namespace Assets.Scripts.Tycoon.RestaurantSystem.TutorialSystem
     using System;
     using System.Collections;
     using UnityEngine;
+    using UnityEngine.UI;
 
     [Serializable]
     public class ExecutableElement : MonoBehaviour, IExcutable
     {
         [SerializeField]
         private float _watingStartTime;
+        [SerializeField]
+        protected float _puaseDuration;
+        [SerializeField]
+        protected Button _skipButton;
+        protected bool _isSkipping;
+        [SerializeField]
+        protected bool isRequiredClickToComplete;
+        protected void OnClickSkipButton()
+        {
+            _isSkipping=true;
+        }
         public virtual IEnumerator Initialize()
         {
             yield return new WaitForSeconds(_watingStartTime);
-            Debug.Log("Called Initialize");
             yield return Begin();
         }
-        public virtual IEnumerator Complete()
+        public virtual IEnumerator Begin()
         {
-            throw new NotImplementedException();
+            if(_skipButton!=null)
+            {
+                _skipButton.gameObject.SetActive(true);
+                _skipButton.onClick.AddListener(OnClickSkipButton);
+            }
+            yield return Next();
+        }
+        public virtual IEnumerator Next()
+        {
+            yield return Execute();
+            yield return Complete();
         }
 
         public virtual IEnumerator Execute()
         {
-            throw new NotImplementedException();
+            if(_isSkipping)
+            {
+                yield return Skip();
+            }
+            yield return Pause();
         }
 
-        public virtual IEnumerator Next()
-        {
-            throw new NotImplementedException();
-        }
 
         public virtual IEnumerator Pause()
         {
-            throw new NotImplementedException();
+            _isSkipping=false;
+            if(isRequiredClickToComplete)
+            {
+                yield return new WaitUntil(()=>_isSkipping);
+                yield break;
+            }
+            float elapsedTime=0f;
+
+            while (elapsedTime < _puaseDuration && !_isSkipping)
+            {
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+            yield break;
         }
 
         public virtual IEnumerator Skip()
         {
-            throw new NotImplementedException();
+            yield break;
         }
 
-        public virtual IEnumerator Begin()
+        public virtual IEnumerator Complete()
         {
-            throw new NotImplementedException();
+            if(_skipButton!=null)
+            {
+                _skipButton.onClick.RemoveListener(OnClickSkipButton);
+                _skipButton.gameObject.SetActive(false);
+            }
+            yield break;
         }
     }
 }
