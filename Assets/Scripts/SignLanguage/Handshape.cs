2a0591270ab53fbbd93c4943817099d1e60f30a2
es-e2b <ensu1997@naver.com>
2024-11-08 14:26:23 +0900

    feature: 수어 손 크기 변경 애니메이션 구현

--- Assets/Scripts/SignLanguage/Handshape.cs
+++ Assets/Scripts/SignLanguage/Handshape.cs
@@ -10,6 +10,8 @@ namespace Assets.Scripts.SignLanguage
         public Vector2 EndPosition;   // 도착 위치
         public Vector3 StartRotation; // 시작 회전
         public Vector3 EndRotation;   // 도착 회전
+        public float StartScale=1f; // 시작 크기
+        public float EndScale=1f;   // 도착 크기
         public float Duration;
     }
 }
