174bac1a30d611d86f76344f839dc99a6a36e8ee
김영찬 <small1258@naver.com>
2024-11-11 03:15:33 +0900

    설정 수정

--- Assets/Scripts/Option.cs
+++ Assets/Scripts/Option.cs
@@ -16,7 +16,7 @@ namespace HandByHand
         private Slider musicSlider;
         
         [SerializeField]
-        private Slider SESlider;
+        private Slider _SESlider;
 
         [SerializeField]
         private Button ResumeButton;
@@ -24,10 +24,32 @@ namespace HandByHand
         [SerializeField]
         private Button MainMenuButton;
 
+        [SerializeField]
+        private Image musicImage;
+
+        [SerializeField]
+        private Image _SEImage;
+
+        [SerializeField]
+        private Sprite muteSprite;
+
+        private Sprite musicSprite;
+        private Sprite _SESprite;
+
+        private float preMusicVolume;
+        private float preSEVolume;
+
+
         private void Start()
         {
             musicSlider.value = SoundManager.Instance.BGMVolume;
-            SESlider.value = SoundManager.Instance.SEVolume;
+            _SESlider.value = SoundManager.Instance.SEVolume;
+
+            preMusicVolume = SoundManager.Instance.BGMVolume;
+            preSEVolume = SoundManager.Instance.SEVolume;
+
+            musicSprite = musicImage.sprite;
+            _SESprite = _SEImage.sprite;
         }
 
         public void SetActiveOptionPanel()
@@ -38,15 +60,80 @@ namespace HandByHand
                 OptionPanel.SetActive(false);
         }
 
+        #region MUSIC
+        public void MuteMusic()
+        {
+            if(musicSlider.value != 0)
+            {
+                musicSlider.value = 0;
+            }
+            else
+            {
+                musicSlider.value = preMusicVolume;
+            }
+
+            AdjustBGMVolume();
+        }
+
         public void AdjustBGMVolume()
         {
             SoundManager.Instance.AdjustBGMVolume(musicSlider.value);
+            CheckBGMMute();
         }
 
+        private void CheckBGMMute()
+        {
+            if(SoundManager.Instance.BGMVolume != 0)
+                preMusicVolume = SoundManager.Instance.BGMVolume;
+
+            if (SoundManager.Instance.BGMVolume == 0)
+            {
+                musicImage.sprite = muteSprite;
+            }
+            else
+            {
+                musicImage.sprite = musicSprite;
+            }
+        }
+        #endregion
+
+        #region SE
+
+        public void MuteSE()
+        {
+            if(_SESlider.value != 0)
+            {
+                _SESlider.value = 0;
+            }
+            else
+            {
+                _SESlider.value = preSEVolume;
+            }
+            
+            AdjustSEVolume();
+        }
+        
         public void AdjustSEVolume()
         {
-            SoundManager.Instance.AdjustSEVolume(SESlider.value);
+            SoundManager.Instance.AdjustSEVolume(_SESlider.value);
+            CheckSEMute();
         }
+
+        private void CheckSEMute()
+        {
+            if(SoundManager.Instance.SEVolume != 0)
+                preSEVolume = SoundManager.Instance.SEVolume;
+
+            if (SoundManager.Instance.SEVolume == 0)
+            {
+                _SEImage.sprite = muteSprite;
+            }
+            else
+            {
+                _SEImage.sprite = _SESprite;
+            }
+        }
+        #endregion
 
         public void ResumeTheGame()
         {
