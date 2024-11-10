6ca800f7a9b574898dfe55540e7077b78306afee
es-e2b <ensu1997@naver.com>
2024-11-10 21:05:34 +0900

    feature: 스타트 씬 executor 적용

--- Assets/Scripts/StartSceneManager.cs
+++ Assets/Scripts/StartSceneManager.cs
@@ -6,33 +6,6 @@ namespace Assets.Scripts
 
     public class StartSceneManager : MonoBehaviour
     {
-        [SerializeField]
-        private ExecutableList _executableList;
-        private void Start()
-        {
-            StartCoroutine(Initialize());
-        }
-        private IEnumerator Initialize()
-        {
-            yield return new WaitUntil(()=>GameManager.Instance!=null);
-            ExecutableElement[] _executables=_executableList.Executables;
-            for(int i=0;i<_executables.Length;i++)
-            {
-                yield return _executables[i].Initialize();
-            }
-        }
-        public void StartExecutableList(ExecutableList executableList)
-        {
-            StartCoroutine(ExecuteExecutableList(executableList));
-        }
-        private IEnumerator ExecuteExecutableList(ExecutableList executableList)
-        {
-            ExecutableElement[] executables=executableList.Executables;
-            for(int i=0;i<executables.Length;i++)
-            {
-                yield return executables[i].Initialize();
-            }
-        }
         public void ChangeScene(DayCycle dayCycle)
         {
             GameManager.Instance.CurrentDayCycle=dayCycle;
