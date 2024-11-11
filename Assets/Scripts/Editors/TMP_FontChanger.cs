10743b1576d5b1f6dd6c9b4e86099f3dc38d8699
es-e2b <ensu1997@naver.com>
2024-11-11 13:59:58 +0900

    Update TMP_FontChanger.cs

--- Assets/Scripts/Editors/TMP_FontChanger.cs
+++ Assets/Scripts/Editors/TMP_FontChanger.cs
@@ -1,48 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using TMPro;
-
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
-
-/*
-https://bonnate.tistory.com/
-
-Insert the script into the game object
-insert the TMP font in the inspector
-and press the button to find and replace all components.
-
-It may work abnormally, so make sure to back up your scene before using it!!
-*/
-
-public class TMP_FontChanger : MonoBehaviour
+namespace Assets.Scripts.Editors
 {
-    [SerializeField] public TMP_FontAsset FontAsset;
-}
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using TMPro;
 
-#if UNITY_EDITOR
-[CustomEditor(typeof(TMP_FontChanger))]
-public class TMP_FontChangerEditor : Editor
-{
-    public override void OnInspectorGUI()
+    #if UNITY_EDITOR
+    using UnityEditor;
+    #endif
+
+    /*
+    https://bonnate.tistory.com/
+
+    Insert the script into the game object
+    insert the TMP font in the inspector
+    and press the button to find and replace all components.
+
+    It may work abnormally, so make sure to back up your scene before using it!!
+    */
+
+    public class TMP_FontChanger : MonoBehaviour
     {
-        base.OnInspectorGUI();
+        [SerializeField] public TMP_FontAsset FontAsset;
+    }
 
-        if (GUILayout.Button("Change Font!"))
+    #if UNITY_EDITOR
+    [CustomEditor(typeof(TMP_FontChanger))]
+    public class TMP_FontChangerEditor : Editor
+    {
+        public override void OnInspectorGUI()
         {
-            TMP_FontAsset fontAsset = ((TMP_FontChanger)target).FontAsset;
+            base.OnInspectorGUI();
 
-            foreach(TextMeshPro textMeshPro3D in GameObject.FindObjectsOfType<TextMeshPro>(true)) 
-            { 
-                textMeshPro3D.font = fontAsset;
-            }
-            foreach(TextMeshProUGUI textMeshProUi in GameObject.FindObjectsOfType<TextMeshProUGUI>(true)) 
-            { 
-                textMeshProUi.font = fontAsset;
+            if (GUILayout.Button("Change Font!"))
+            {
+                TMP_FontAsset fontAsset = ((TMP_FontChanger)target).FontAsset;
+
+                foreach(TextMeshPro textMeshPro3D in GameObject.FindObjectsOfType<TextMeshPro>(true)) 
+                { 
+                    textMeshPro3D.font = fontAsset;
+                    textMeshPro3D.fontStyle = FontStyles.Normal;
+                }
+                foreach(TextMeshProUGUI textMeshProUi in GameObject.FindObjectsOfType<TextMeshProUGUI>(true)) 
+                { 
+                    textMeshProUi.font = fontAsset;
+                    textMeshProUi.fontStyle = FontStyles.Normal;
+                }
             }
         }
     }
+    #endif
 }
-#endif
