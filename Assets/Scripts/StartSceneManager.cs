7be6b11fb00cdd2667f58581a260bc7bec21e835
김영찬 <small1258@naver.com>
2024-11-12 00:55:50 +0900

    feature : CreditOnOff함수 추가

--- Assets/Scripts/StartSceneManager.cs
+++ Assets/Scripts/StartSceneManager.cs
@@ -6,9 +6,24 @@ namespace Assets.Scripts
 
     public class StartSceneManager : MonoBehaviour
     {
+        [SerializeField]
+        private GameObject CreditPanel;
+
         public void ChangeScene(DayCycle dayCycle)
         {
             GameManager.Instance.CurrentDayCycle=dayCycle;
+        }
+
+        public void CreditOnOff()
+        {
+            if(CreditPanel.activeSelf)
+            {
+                CreditPanel.SetActive(false);
+            }
+            else
+            {
+                CreditPanel.SetActive(true);
+            }
         }
     }
 }
